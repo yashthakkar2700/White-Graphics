d9ab41e46192a20598ac86564935972845057448
Yash Thakkar <yashthakkar2700@gmail.com>
2019-12-06 21:05:38 +0530

    Completed Home page

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@
  * Contains Script for basic static website named "White Graphics"
  */
 $(window).on('load', function(){
-    $("#preloader").delay(6000).fadeOut("slow");
+    $("#preloader").delay(100).fadeOut("slow");
 });
