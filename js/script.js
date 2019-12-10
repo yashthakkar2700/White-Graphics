e4f13c2803fadf6d5989a03d57b2752e045d596c
Yash Thakkar <yashthakkar2700@gmail.com>
2019-12-11 00:26:57 +0530

    Completed Testimonials, Pricing, Stats, Clients and Blog section

--- js/script.js
+++ js/script.js
@@ -89,12 +89,95 @@ $(document).ready(function(){
    }) 
 });
 
+/************************************************************************
+                        TESTIMONIAL SECTION
+************************************************************************/
 
+$(document).ready(function(){
+   $("#testimonial-slider").owlCarousel({
+       items: 1,
+       autoplay: true,
+       margin: 20,
+       loop: true,
+       nav: true,
+       smartspeed: 700,
+       autoplayHoverPause: true,
+       dots: false,
+       navText: ['<i class="lni-chevron-left-circle"></i>', '<i class="lni-chevron-right-circle"></i>']
+   });               
+});
 
+/************************************************************************
+                        STATS SECTION
+************************************************************************/
 
+$(document).ready(function(){
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
 
+/************************************************************************
+                        CLENTS SECTION
+************************************************************************/
 
+$(document).ready(function(){
+   $("#client-list").owlCarousel({
+       items: 6,
+       autoplay: true,
+       margin: 20,
+       loop: true,
+       nav: true,
+       smartspeed: 700,
+       autoplayHoverPause: true,
+       dots: false,
+       navText: ['<i class="lni-chevron-left-circle"></i>', '<i class="lni-chevron-right-circle"></i>'],
+       
+       responsive: {
+           0:{
+               items: 2,
+               nav: false
+           },
+            480:{
+               items: 4,
+                nav: false
+           },
+            768:{
+               items: 6
+           }
+       }
+   });               
+});
 
+/************************************************************************
+                        GOOGLE MAPS
+************************************************************************/
+
+$(window).on('load', function(){
+   var addressString = "302, Evergreen CHS,<br>Airoli, Maharashtra, <br>India";
+    var myLatLng = {
+      lat: 19.173829,
+      lng: 72.953716
+    };
+    var myMap = new google.maps.Map(document.getElementById('map'),{
+       zoom: 17,
+        center: myLatLng
+    });
+    
+    var marker = new google.maps.Marker({
+       position: myLatLng,
+        map: myMap,
+        title: "Click to see address!"
+    });
+    
+    var infoWindow = new google.maps.InfoWindow({
+       content: addressString 
+    });
+    marker.addListener('click', function(){
+        infoWindow.open(myMap, marker);
+    })  
+});
 
 
 
